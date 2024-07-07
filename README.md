bc8169275aa9454076a1f6952d537cd4fae012a7
marco <marcoranalli22@gmail.com>
2024-07-07 18:58:58 +0200

    feat:backend working

--- README.md
+++ README.md
@@ -1,3 +1,98 @@
 # Random Number Generator
 
 This is a simple fullstack random number generator program developed by @marcoranalli (me) during the Lacerba node and typescript development course.
+
+## Install
+
+```bash
+npm install
+```
+
+## Usage backend
+
+```bash
+ts-node main.ts
+```
+
+example output:
+```
+{
+  "Bari": [
+    12,
+    38,
+    74,
+    95,
+    44
+  ],
+  "Cagliari": [
+    75,
+    30,
+    85,
+    45,
+    39
+  ],
+  "Firenze": [
+    79,
+    74,
+    12,
+    15,
+    38
+  ],
+  "Genova": [
+    84,
+    75,
+    57,
+    1,
+    70
+  ],
+  "Milano": [
+    99,
+    72,
+    23,
+    49,
+    29
+  ],
+  "Napoli": [
+    11,
+    76,
+    83,
+    29,
+    82
+  ],
+  "Palermo": [
+    98,
+    22,
+    95,
+    48,
+    71
+  ],
+  "Roma": [
+    54,
+    29,
+    77,
+    93,
+    16
+  ],
+  "Torino": [
+    57,
+    32,
+    53,
+    82,
+    98
+  ],
+  "Venezia": [
+    12,
+    96,
+    50,
+    21,
+    11
+  ],
+  "Nazionale": [
+    74,
+    97,
+    20,
+    38,
+    39
+  ]
+}
+```
--- main.ts
+++ main.ts
@@ -1,1 +1,57 @@
-console.log('hello');
+function RNG(min:number, max:number) {
+    const rng = Math.random();
+    return Math.trunc(rng * (max - min) + min);
+}
+
+function RNGSequence(len:number, min:number, max:number) {
+    if(len > max - min) {
+        throw new Error(`cannot find ${len} numbers between ${min} and ${max}`);
+    }
+    const res: number[] = [];
+    while (res.length < len) {
+        const rn = RNG(min, max);
+        if(res.includes(rn)) {
+            continue;
+        }
+        res.push(rn);
+    }
+    return res
+}
+
+const ruote = [
+    'Bari',
+    'Cagliari',
+    'Firenze',
+    'Genova',
+    'Milano',
+    'Napoli',
+    'Palermo',
+    'Roma', 
+    'Torino', 
+    'Venezia', 
+    'Nazionale'
+];
+
+const estrazioni: { [ruota: string]: number[] } = {} // chiave: stringa, valore: numeri in array
+
+for(let ruota of ruote) {
+    const estrazione = RNGSequence(5, 1, 100);
+    estrazioni[ruota] = estrazione;
+}
+
+console.log(JSON.stringify(estrazioni, null, 2));
+
+// CODING CHALLANGE
+
+// function RNGDec(min:number, max:number, precision:number) {
+//     if(precision < 0) {
+//         throw new Error(`Error: precision must be a positive number`);
+//     } else if(!Number.isInteger(precision)) {
+//         throw new Error(`Error: precision must be an integer number`);
+//     }
+//     const multFactor = Math.pow(10, precision);
+//     return RNG(min * multFactor, max * multFactor) /multFactor;
+// }
+
+// console.log(RNGDec(5, 10, 2.1));
+
